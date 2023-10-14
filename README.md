cb7c580d8af7eb45f68826a22fd602b898b338fd
ChenZiQiang <ziqiang.chen@outlook.com>
2023-10-15 08:53:48 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# To-Do List
+
+## Technology Stack / 技術スタック
+
++ Next.js 13 (App Router)
++ TypeScript
++ Material UI
++ MongoDB
---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import styles from './page.module.css'
+
+export default function Home() {
+
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getHello() {
+      const resp = await fetch("/api/ping");
+      const res = await resp.text();
+      setMsg(res);
+      setLoading(false);
+    }
+    getHello();
+  }, [])
+
+  if (loading) {
+    return <h1 className={styles.h1}>Loading...</h1>
+  }
+
+  return <h1 className={styles.h1}>{msg}</h1>
+}
